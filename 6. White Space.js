d7c1dfc3f98f2ab22c74033e8038423b760e13b4
Syeda Batool Zehra <syedabatoolzehra409@gmail.com>
2024-06-01 22:39:26 +0500

    Add files via upload

---
+++ 6. White Space.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let name1 = "\tJohn Doe\n";
+console.log("Name with whitespace:", name1);
+console.log("Name after stripping whitespace:", name1.trim());
---
+++ 6. White Space.ts
@@ -0,0 +1,4 @@
+let name1= "\tJohn Doe\n"
+console.log("Name with whitespace:" , name1)
+console.log("Name after stripping whitespace:" , name1.trim())
+
